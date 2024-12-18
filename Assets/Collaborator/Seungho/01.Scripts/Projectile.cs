0ddbe7517da582c18aced88c2ccbba89df72c513
jiho08 <57872222k@gmail.com>
2024-12-19 01:29:25 +0900

    fix : weapon

--- Assets/Collaborator/Seungho/01.Scripts/Projectile.cs
+++ Assets/Collaborator/Seungho/01.Scripts/Projectile.cs
@@ -7,21 +7,20 @@ public class Projectile : MonoBehaviour
     TrailRenderer tr;
     public float speed;
     public Transform owner;
-    
+    public bool isPenetration;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         tr = GetComponent<TrailRenderer>();
-        owner = transform;   
-        
+        owner = transform;
     }
 
     private void OnEnable()
     {
         tr.Clear();
         rb.linearVelocity = Vector3.zero;
-        
         rb.AddForce(owner.transform.right * speed, ForceMode2D.Impulse);
     }
 
@@ -30,7 +29,10 @@ public class Projectile : MonoBehaviour
         if (collision.gameObject.CompareTag("Wall"))
         {
             PoolManager.Return(0, gameObject);
-            
+        }
+        else if (collision.gameObject.CompareTag("Player") && !isPenetration)
+        {
+            PoolManager.Return(0, gameObject);
         }
     }
 }
