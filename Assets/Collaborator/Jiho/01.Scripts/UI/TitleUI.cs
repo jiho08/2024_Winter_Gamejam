36e653846b8b07e996c032420402785c546c6a78
jiho08 <57872222k@gmail.com>
2024-12-19 15:51:49 +0900

    feat : sound volume playerprefs

--- Assets/Collaborator/Jiho/01.Scripts/UI/TitleUI.cs
+++ Assets/Collaborator/Jiho/01.Scripts/UI/TitleUI.cs
@@ -1,36 +1,71 @@
+using System;
 using System.Collections.Generic;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Hellmade.Sound;
+using UnityEngine.UI;
 
 public class TitleUI : MonoBehaviour
 {
     [SerializeField] private List<CinemachineCamera> cameraList = new();
+    [SerializeField] private List<Slider> sliderList = new();
+
+    private void Start()
+    {
+        var masterValue = PlayerPrefs.GetFloat("Master", 0.5f);
+        var musicValue = PlayerPrefs.GetFloat("Music", 0.5f);
+        var effectValue = PlayerPrefs.GetFloat("Effect", 0.5f);
+
+        sliderList[0].value = masterValue;
+        sliderList[1].value = musicValue;
+        sliderList[2].value = effectValue;
+        
+        EazySoundManager.GlobalVolume = masterValue;
+        EazySoundManager.GlobalMusicVolume = musicValue;
+        EazySoundManager.GlobalSoundsVolume = effectValue;
+        EazySoundManager.GlobalUISoundsVolume = effectValue;
+    }
 
     public void StartButton()
     {
-        // 스테이지 선택 창으로
-        Debug.Log("시작 버튼");
-
         (cameraList[0].Priority, cameraList[2].Priority) = (cameraList[2].Priority, cameraList[0].Priority);
     }
 
     public void SettingButton()
     {
-        // 설정 창으로
-        Debug.Log("설정 버튼");
         (cameraList[0].Priority, cameraList[1].Priority) = (cameraList[1].Priority, cameraList[0].Priority);
     }
 
     public void ExitButton()
     {
-        Debug.Log("나가기");
         Application.Quit();
     }
-    
+
     public void BackButton()
     {
-        Debug.Log("두ㅣ로");
         (cameraList[0].Priority, cameraList[1].Priority) = (cameraList[1].Priority, cameraList[0].Priority);
+    }
+
+    public void MasterSliderValueChanged(float value)
+    {
+        Debug.Log($"마스터 볼륨 : {value}");
+        PlayerPrefs.SetFloat("Master", value);
+        EazySoundManager.GlobalVolume = value;
+    }
+
+    public void MusicSliderValueChanged(float value)
+    {
+        Debug.Log($"뮤직 볼륨 : {value}");
+        PlayerPrefs.SetFloat("Music", value);
+        EazySoundManager.GlobalMusicVolume = value;
+    }
+
+    public void EffectSliderValueChanged(float value)
+    {
+        Debug.Log($"이펙트 볼륨 : {value}");
+        PlayerPrefs.SetFloat("Effect", value);
+        EazySoundManager.GlobalSoundsVolume = value;
+        EazySoundManager.GlobalUISoundsVolume = value;
     }
 }
